131fa21ef1a387032717da7c04f19549cbf78349
Snorre Stenerud <snorre.stenerud@gmail.com>
2024-01-28 14:43:32 +0100

    customers

---
+++ shop.shared/Customer.cs
@@ -0,0 +1,23 @@
+﻿using shop.eventsourcing;
+
+namespace shop.shared;
+public record Customer(Guid Id, string Name, string Address) : DomainModel(Id);
+
+public record NewCustomerEvent(Guid CustomerId, string Name, string Address, DateTimeOffset AppliesAt) : Event<Customer>(CustomerId, AppliesAt)
+{
+    public override Customer? Apply(Customer? existing)
+    {
+        return new Customer(CustomerId, Name, Address);
+    }
+}
+
+public record UpdateCustomerAddressEvent(Guid CustomerId, string? Address, DateTimeOffset AppliesAt) : Event<Customer>(CustomerId, AppliesAt)
+{
+    public override Customer? Apply(Customer? existing)
+    {
+        return existing is null ? null : existing with
+        {
+            Address = Address is null ? existing.Address : Address
+        };
+    }
+}
--- shop.shared/SharedServiceCollectionExtensions.cs
+++ shop.shared/SharedServiceCollectionExtensions.cs
@@ -7,8 +7,6 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class SharedServiceCollectionExtensions
 {
-    private static DateTimeOffset currentDateTimeOffset = DateTimeOffset.Now.AddDays(-10);
-
     public static IServiceCollection AddSharedDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("ShopDb");
@@ -22,37 +20,10 @@ public static class SharedServiceCollectionExtensions
 
         services
             .AddTransient<IEventStore, LocalDbEventStore>()
-            .AddTransient<IDomainService<Fruit>, DomainService<Fruit>>();
+            .AddTransient<IDomainService<Fruit>, DomainService<Fruit>>()
+            .AddTransient<IDomainService<Customer>, DomainService<Customer>>()
+            ;
 
         return services;
-    }
-
-    private static List<DomainEvent> FruitEvents()
-    {
-        List<DomainEvent> fruitEvents = [];
-
-        var appleId = "Apple".ToGuid();
-        fruitEvents.Add(new CreateFruitEvent(appleId, "Apple", "Red", EventDate()));
-        fruitEvents.Add(new UpdateFruitEvent(appleId, "Green", EventDate()));
-        fruitEvents.Add(new DeleteFruitEvent(appleId, EventDate()));
-
-        var bananaId = "Banana".ToGuid();
-        fruitEvents.Add(new CreateFruitEvent(bananaId, "Banana", "Yellow", EventDate()));
-        fruitEvents.Add(new UpdateFruitEvent(bananaId, "Orange", EventDate()));
-
-
-        var kiwiId = "Kiwi".ToGuid();
-        fruitEvents.Add(new CreateFruitEvent(kiwiId, "Kiwi", "Yellow", EventDate()));
-        fruitEvents.Add(new UpdateFruitEvent(kiwiId, "Orange", EventDate()));
-
-        return fruitEvents;
-    }
-
-    private static DateTimeOffset EventDate()
-    {
-        // Adding one day to the input DateTimeOffset
-        currentDateTimeOffset = currentDateTimeOffset.AddDays(1);
-
-        return currentDateTimeOffset;
     }
 }
