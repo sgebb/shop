f176dd605faf2c33d09a3b106c974558df79ef29
Snorre Stenerud <snorre.stenerud@gmail.com>
2024-02-04 13:45:42 +0100

    merge

---
+++ shop.api/Migrations/20240204123908_Initial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shop.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IEvents",
+                columns: table => new
+                {
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ModelId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DomainModelType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IEvents", x => x.EventId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReadModels",
+                columns: table => new
+                {
+                    ReadModelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    At = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DomainModelType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReadModels", x => x.ReadModelId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IEvents");
+
+            migrationBuilder.DropTable(
+                name: "ReadModels");
+        }
+    }
+}
